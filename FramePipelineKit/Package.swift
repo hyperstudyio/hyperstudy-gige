00e978ea1f24c110d956b95302d26b543ae75aec
ljchang <lukejchang@gmail.com>
2026-05-20 00:33:40 -0400

    fix(pipeline): lower FramePipelineKit deployment target to macOS 12 to match app

--- FramePipelineKit/Package.swift
+++ FramePipelineKit/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "FramePipelineKit",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v12)],
     products: [
         .library(name: "FramePipelineKit", targets: ["FramePipelineKit"])
     ],
